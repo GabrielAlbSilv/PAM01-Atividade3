909d5ca230f96fb12d61b4b2573a85f44f6387e1
GabrielAlbSilv <gabrielalbuquequemoura@gmail.com>
2021-10-06 20:15:10 -0300

    java commit

--- app/src/main/java/br/com/etecia/exercicio2/MainActivity.java
+++ app/src/main/java/br/com/etecia/exercicio2/MainActivity.java
@@ -1,5 +1,6 @@
 package br.com.etecia.exercicio2;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -12,12 +13,23 @@ import android.view.View;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnCads;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnCads = findViewById(R.id.btnCads);
+
+        btnCads.setOnClickListener(new View.OnClickListener() { //ação do botão Sig in ir para tela login
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Cadastro.class);
+                startActivity(intent);
+            }
+        });
     }
 }
