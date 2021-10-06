459d1b5f5ed23bfbcdc6d84c30d280342f6d2cef
GabrielAlbSilv <gabrielalbuquequemoura@gmail.com>
2021-10-06 20:09:49 -0300

    parte estética commit

---
+++ app/src/main/java/br/com/etecia/exercicio2/Cadastro.java
@@ -0,0 +1,14 @@
+package br.com.etecia.exercicio2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Cadastro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_cadastro);
+    }
+}
